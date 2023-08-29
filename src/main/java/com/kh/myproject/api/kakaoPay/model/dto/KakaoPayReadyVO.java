970b1303b96bc4957b3b622317889b72c339402c
sungsick <kalo2218@gmail.com>
2023-08-29 14:42:48 +0900

    front update

--- src/main/java/com/kh/myproject/api/kakaoPay/model/dto/KakaoPayReadyVO.java
+++ src/main/java/com/kh/myproject/api/kakaoPay/model/dto/KakaoPayReadyVO.java
@@ -1,8 +1,10 @@
 package com.kh.myproject.api.kakaoPay.model.dto;
 
-import java.util.Date;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import lombok.*;
+import java.util.Date;
 
 @Data
 @Getter
