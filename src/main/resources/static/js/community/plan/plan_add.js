174bc450fc542404cf1f4a674afa8d40db15c23c
JUNGWOOKCHOI <cjw9796@gmail.com>
2023-08-30 12:50:44 +0900

    jungwook2 -> jungwook

--- src/main/resources/static/js/community/plan/plan_add.js
+++ src/main/resources/static/js/community/plan/plan_add.js
@@ -16,7 +16,7 @@ var item = []; //테스트
 // 마커를 담을 배열입니다
 var markers = [];
 
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div.
     mapOption = {
         center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
