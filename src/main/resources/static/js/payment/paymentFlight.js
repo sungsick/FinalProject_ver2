3fb2443ebf81c7734543a13dfbe04c8f00c6e03d
sungsick <kalo2218@gmail.com>
2023-08-31 14:02:48 +0900

    병합 전 커밋

--- src/main/resources/static/js/payment/paymentFlight.js
+++ src/main/resources/static/js/payment/paymentFlight.js
@@ -16,26 +16,23 @@ $(function () {
     });
 
 
-// 대여 가능 여부 생년 검사
+    // 대여 가능 여부 생년 검사
     $('#input_birth').keyup(() => {
 
         var input_birth = document.getElementById('input_birth');
         var warningDiv = document.getElementById('warning');
-        var inputYear = parseInt(input_birth.value.substring(0, 2));
+        var inputYear = input_birth.value.substring(0, 2);
         var currentYear = new Date().getFullYear() % 100;
-        const birthpattern = /^(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0,1])$/;
         var btnKakaoPay = document.getElementById('btnKakaoPay');
 
         if (input_birth.value >= 2) {
             var inputValue = parseInt(input_birth.value.substring(0, 2));
         }
-
         console.log(input_birth.value.length);
+        console.log(inputYear);
         if (input_birth.value.length >= 6) {
 
-            console.log(currentYear - inputYear);
             result = currentYear - inputYear;
-            console.log(result);
             absolute = Math.abs(result);
             console.log(absolute);
 
@@ -53,46 +50,58 @@ $(function () {
                 btnKakaoPay.disabled = true;
             }
 
-        } else if (!birthpattern.test(input_birth.value)) {
-            // 정규식 제대로 작동 안함. 이전에 조건에 의해 조건이 끝나는듯
-            console.log(birthpattern.test(input_birth.value) || input_birth.value.length > 6);
-            warningDiv.textContent = "정확한 생년월일을 입력 하세요.";
-            warningDiv.style.color = "red";
-            btnKakaoPay.disabled = true;
         } else {
             warningDiv.textContent = "'-' 을 제외한 생년월일 6자리를 입력하세요"; // 경고 메시지 초기화
             btnKakaoPay.disabled = true;
         }
     });
 
-    // 카카오결제
+// 카카오결제
     $("#btnKakaoPay").click(function () {
 
         var query = {
             input_name: $("#input_name").val(),
             input_phone: $("#input_phone").val(),
             input_birth: $("#input_birth").val(),
+            monthDay: $("#input_birth").val().substring(2),
             checkVal: $("input[formcontrolname=gender]:checked")
         }
-        const phoneNumberPattern = /^01([0|1|6|7|8|9]?)([0-9]{3,4})([0-9]{4})$/; // 휴대폰 형식검사 정규 표현식
+        const phoneNumberPattern = /^01[0-9]-\d{3,4}-\d{4}$/; // 휴대폰 형식검사 정규 표현식
+        const birthpattern = /^(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0,1])$/;
+        var btnKakaoPay = document.getElementById('btnKakaoPay');
+        var warningDiv = document.getElementById('warning');
 
         if (query.input_name === '') {
             alert('이름을 입력하세요.');
-            query.input_name.focus();
+            $("#input_name").focus();
+            btnKakaoPay.disabled = true;
         } else if (query.input_phone === '') {
             alert('핸드폰 번호를 입력하세요.');
-            query.input_birth.focus();
-        } else if (iquery.input_birth === '') {
+            $("#input_phone").focus();
+            btnKakaoPay.disabled = true;
+        }
+            // 핸드폰 인증으로 대체
+            // else if (!phoneNumberPattern.test(query.input_phone.value)) {
+            //     alert("휴대폰 번호를 올바르게 입력하세요.");
+            //     $("#input_phone").focus();
+            //     btnKakaoPay.disabled = true;
+        // }
+        else if (query.input_birth === '') {
             alert('생년월일을 입력하세요.');
-            query.input_birth.focus();
-        } else if (!phoneNumberPattern.test(query.input_phone.value)) {
-            alert("휴대폰 번호를 올바르게 입력하세요.");
-            query.input_birth.focus();
+            $("#input_birth").focus();
+            btnKakaoPay.disabled = true;
+        } else if (!birthpattern.test(query.monthDay)) {
+            console.log(birthpattern.test(input_birth.value));
+            warningDiv.textContent = "정확한 생년월일을 입력 하세요.";
+            warningDiv.style.color = "red";
+            btnKakaoPay.disabled = true;
+            $("#input_birth").focus();
         } else if (!query.checkVal) {
             alert("성별을 선택해 주세요.");
-            focus(input[formcontrolname = gender]);
+            btnKakaoPay.disabled = true;
+            $("input[formcontrolname='gender']:checked").focus();
         } else {
-
+            btnKakaoPay.disabled = false;
             // 필수입력값을 확인.
             var name = $("#form-payment input[name='pay-name']").val();
             var tel = $("#form-payment input[name='pay-tel']").val();
@@ -148,14 +157,14 @@ $(function () {
         }
     })
 
-    // 모달 열기
+// 모달 열기
     $('.shuttle-button').click(function () {
         $('.modal').css('display', 'block');
         $("body").css('overflow', 'hidden');
         // $("body").css('margin-left', '1px');  // 스크롤로 인한 화면 꿀렁거림 제거
         // 이유 모르겠는데 위 현상 없어짐
     })
-    // 모달 닫기
+// 모달 닫기
     $('.modal-btn').click(function () {
         $('.modal').css('display', 'none');
         $("body").css('overflow', 'auto');
@@ -163,7 +172,7 @@ $(function () {
         // 이유 모르겠는데 위 현상 없어짐
     })
 
-    // 텍스트 숨기기
+// 텍스트 숨기기
     $('.arrow').click(function () {
         if ($(this).hasClass('false')) {
             $('.arrow').removeClass('false');
@@ -178,7 +187,7 @@ $(function () {
         }
     })
 
-    // 내륙, 제주 지역 선택텝
+// 내륙, 제주 지역 선택텝
     var $landLocation = $('.insurance-locations div').click(function () {
         var idx = $landLocation.index(this);
         $('.insurance-locations div').removeClass('locationClick');
@@ -188,7 +197,8 @@ $(function () {
         $('.sections-con.show > section:nth-child(1) > table').eq(idx).addClass('showTale');
     });
 
-});
+})
+;
 // if (idx === 0) {
 //     $('.second-box-tabs-clicked-bar').css('margin-left', '0px')
 // } else if (idx === 1) {
