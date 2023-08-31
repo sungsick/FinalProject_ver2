7d0bdbc5f9f941e806da9d69fbc911ee27a034eb
주미 <jumi4820@gmail.com>
2023-09-01 08:57:33 +0900

    백업

--- src/main/java/com/kh/myproject/store/rentcar/cotroller/RentcarController.java
+++ src/main/java/com/kh/myproject/store/rentcar/cotroller/RentcarController.java
@@ -1,35 +1,81 @@
 package com.kh.myproject.store.rentcar.cotroller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
+@Slf4j
 @SessionAttributes("user")
 public class RentcarController {
 
 
 
 
-@GetMapping("/store/rentcar/rentcarMain")
+/* @GetMapping("/store/rentcar/rentcarMain")
     public String rentcarMain(){
 
     return "store/rentcar/rentcarMain";
 
-}
+}*/
 
 
-@GetMapping("/rentcarReserve")
-    public String rentcarReserve(){
+    @GetMapping("/store/rentcar/rentcarMain")
+    public ModelAndView rentcarMain(ModelAndView mav) {
 
-    return "store/rentcar/rentcarReserve";
-}
+        mav.setViewName("store/rentcar/rentcarMain");
+
+        return mav;
+    }
 
 
-    @GetMapping("/rentcarChoice")
-    public String rentcarChoice(){
+    @GetMapping("/store/rentcar/rentcarReserve")
+    public ModelAndView rentcarReserve(ModelAndView mav) {
 
-        return "store/rentcar/rentcarChoice";
+        mav.setViewName("store/rentcar/rentcarReserve");
+
+        return mav;
+    }
+
+    @GetMapping("/store/rentcar/rentcarChoice")
+    public ModelAndView rentcarChoice(ModelAndView mav) {
+
+        mav.setViewName("store/rentcar/rentcarChoice");
+
+        return mav;
+    }
+
+
+
+
+
+
+    //main에서 검색버튼 누른 후 reserve로 넘어가는 메서드
+    @GetMapping("/store/rentcar/rentcarMainSearch")
+    public ModelAndView mainSearch(        @RequestParam("location") String location,
+                                           @RequestParam("departDate") String departDate,
+                                           @RequestParam("arriveDate") String arriveDate,
+                                           @RequestParam("birthDate") String birthDate,
+                                           ModelAndView mav){
+
+        log.info("location", location);
+        log.info("departDate",departDate);
+        log.info("arriveDate",arriveDate);
+        log.info("birthDate",birthDate);
+
+
+
+        mav.addObject("location", location);
+        mav.addObject("departDate", departDate);
+        mav.addObject("arriveDate", arriveDate);
+        mav.addObject("birthDate", birthDate);
+
+
+        mav.setViewName("store/rentcar/thTest");
+
+        return mav;
+
     }
 
 
